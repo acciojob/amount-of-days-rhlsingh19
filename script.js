e5e827ceb12573c9837de348458068814c3e326b
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-01 15:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return 366; // Leap year has 366 days
+  } else {
+    return 365; // Non-leap year has 365 days
+  }
+}
